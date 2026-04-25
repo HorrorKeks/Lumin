ce3c840650c882331faa67c41c093f2c5fbf0357
HorrorKeks <lueneburg.1@icloud.com>
2026-04-25 19:39:14 +0200

    Add files via upload

---
+++ lumin/frontend/src/components/ChannelList.jsx
@@ -0,0 +1,8 @@
+export default function ChannelList() {
+  return (
+    <div className="w-64 bg-[#2b2d31] p-3">
+      <h2 className="text-gray-300"># general</h2>
+      <h2 className="text-gray-400"># memes</h2>
+    </div>
+  );
+}
